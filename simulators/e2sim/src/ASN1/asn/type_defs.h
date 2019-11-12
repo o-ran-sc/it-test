fe3e1e705166f084a5d13cd982a1b36b1b1c6827
Bharath <bharathb@research.att.com>
2019-11-12 14:42:29 -0500

    change header to add copyright notice
    Signed-off-by: Bharath <bharathb@research.att.com>
    Change-Id: I68554b0e790e721a8d3334bf2ac94c50360ed4d1

--- simulators/e2sim/src/ASN1/asn/type_defs.h
+++ simulators/e2sim/src/ASN1/asn/type_defs.h
@@ -1,20 +1,22 @@
 #pragma once
-/*******************************************************************************
- * @file                  $HeadURL: $
- * @version               $LastChangedRevision: $
- * @date                  $LastChangedDate: $
- * @author                ngf684
- *
- * @brief                 /iphy_debug/src/common/type_defs.h:
- *
- * Revision History:
- *  01.06.2012  ngf684          created
- *
- * Copyright Nokia 2015. All rights reserved.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND
- *
- *******************************************************************************/
+/******************************************************************************
+*
+*   Copyright (c) 2019 AT&T Intellectual Property.
+*   Copyright (c) 2018-2019 Nokia.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*
+*******************************************************************************/
 
 // Standard Includes: ANSI C/C++, MSA, and Third-Party Libraries
 #include <cstdint>
