0ecb83876534057c87d21675c7b5fa773d77a828
dave kormann <dk3239@att.com>
2019-11-12 14:08:44 -0500

    Update release notes and remove ONS demo tree
    This patch removes the ONS 2019 demo subtree and provides an initial
    skeletal release-notes file.
    
    signed-off-by: dave kormann <dk3239@att.com>
    Change-Id: I6efc52aaa3284baa066abcec4c291e208d77a392

--- ons_2019_demo/pendulum_scripts/kill_scripts/kill_serial_listener.sh
+++
@@ -1,1 +0,0 @@
-pkill -f serial_listener
--- ons_2019_demo/segway_scripts/kill_scripts/kill_ric_and_dashboard.sh
+++
@@ -1,6 +0,0 @@
-pkill -f pendulum_xapp
-pkill -f e2sim #kills both agent and term
-pkill -f dummy #kills a1 med C code and load consumer
-pkill -f load_gen
-pkill -f a1med #kills a1 mediator http server
-docker kill $(docker ps -q) #kills dashboard
