e275ff0ac796b49eeed92d8833b0d4bf470ae791
pceicicd <pekwatch746@gmail.com>
2023-06-27 18:23:41 +0000

    update the RIC deployment to use the latest stable release rather than hardcoding to a given release
    Change-Id: I80ed0b1e6ea2b904debdca881f652d0118c6c2d2
    Signed-off-by: pceicicd <pekwatch746@gmail.com>

--- XTesting/richelm/deploy.sh
+++ XTesting/richelm/deploy.sh
@@ -4,4 +4,4 @@ set -x
 git clone "https://gerrit.o-ran-sc.org/r/ric-plt/ric-dep"
 
 export VERIFY_CHECKSUM=false
-cd ric-dep/bin && ./verify-ric-charts  && cat ../RECIPE_EXAMPLE/example_recipe_oran_f_release.yaml | sed -e 's/10\.0\.0\.1//g' > ../RECIPE_EXAMPLE/example_recipe_oran_f_release.yaml.overwrite && cat install | sed -e 's/bash/bash -x/' | sed -e 's/helm install/helm install --debug/' > install2 && chmod +x install2 && ./install2 -f ../RECIPE_EXAMPLE/example_recipe_oran_f_release.yaml.overwrite
+cd ric-dep/bin && ./verify-ric-charts  && cat ../RECIPE_EXAMPLE/example_recipe_latest_stable.yaml | sed -e 's/10\.0\.0\.1//g' > ../RECIPE_EXAMPLE/example_recipe_latest_stable.yaml.overwrite && cat install | sed -e 's/bash/bash -x/' | sed -e 's/helm install/helm install --debug/' > install2 && chmod +x install2 && ./install2 -f ../RECIPE_EXAMPLE/example_recipe_latest_stable.yaml.overwrite
